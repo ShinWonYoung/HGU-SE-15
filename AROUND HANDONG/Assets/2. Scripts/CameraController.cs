3dae11f72eef8b16f17f08a1956a9925c90cd207
JackCme <pitou_106@naver.com>
2015-11-13 11:08:03 +0900

    Revert "Revert "CameraController""
    This reverts commit 8af1f3eabe03daa2cdd64394832d4b9fc65b2e6c.

---
+++ AROUND HANDONG/Assets/2. Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public float movX;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //스크립트가 적용된 오브젝트의 transform 의 x 값을 시간과 속도값을 곱해줌으로써
+        //오른쪽으로 움직이게 함.
+        movX = transform.position.x + (speed * Time.deltaTime);
+        transform.position = new Vector3(movX, transform.position.y, transform.position.z);
+	}
+}
