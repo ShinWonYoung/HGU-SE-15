c3d7a7fd42bd25f07ca513905dd96340d4833fb9
ShinWonYoung <rebeccawyshin@gmail.com>
2015-12-01 10:31:34 +0900

    Obstacle Addition

---
+++ AROUND HANDONG/Assets/2. Scripts/MouseOneMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseOneMovement : MonoBehaviour
+{
+
+    private float speed = -5.0f;
+    private float moveX;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = transform.position.x + (speed * Time.deltaTime);
+        transform.position = new Vector3(moveX, transform.position.y, transform.position.z);
+
+        if(moveX <= 36.3)
+        {
+            speed = -speed;
+        }
+        else if(moveX >= 51.5)
+        {
+            speed = -speed;
+        }
+    }
+}
---
+++ AROUND HANDONG/Assets/2. Scripts/MouseTwoMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTwoMovement : MonoBehaviour {
+
+    private float speed = -5.0f;
+    private float moveX;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = transform.position.x + (speed * Time.deltaTime);
+        transform.position = new Vector3(moveX, transform.position.y, transform.position.z);
+
+        if (moveX <= 72.3)
+        {
+            speed = -speed;
+        }
+        else if (moveX >= 87.5)
+        {
+            speed = -speed;
+        }
+    }
+}
